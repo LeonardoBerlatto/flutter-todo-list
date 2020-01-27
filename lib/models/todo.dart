fc247d860b48c45e21201dd82854cf10d1907b5b
leonardo-berlatto <leoberlatto@gmail.com>
2020-01-26 22:02:14 -0300

    ✨ Adding ToDo model

---
+++ lib/models/todo.dart
@@ -0,0 +1,13 @@
+class ToDo {
+  
+  String _title;
+  bool _done;
+
+  get title => this._title;
+
+  set title(String title) => this._title = title;
+
+  get done => this._done;
+
+  set done(bool done) => this._done = done;
+}
