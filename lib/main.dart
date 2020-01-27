e32b79e9a900cb653bc57887dd0b81ff731086e8
leonardo-berlatto <leoberlatto@gmail.com>
2020-01-26 22:09:56 -0300

    💩 Adding functions to deal with files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'models/todo.dart';
 
 void main() => runApp(ToDoApp());
 
@@ -18,6 +24,19 @@ class AppHome extends StatefulWidget {
 }
 
 class _HomeState extends State<AppHome> {
+  List<ToDo> _todoList = [];
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/tasks-data.json');
+  }
+
+  Future<File> _saveFile() async {
+    String encodedJsonList = json.encode(_todoList);
+    final File file = await _getFile();
+    return file.writeAsString(encodedJsonList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
